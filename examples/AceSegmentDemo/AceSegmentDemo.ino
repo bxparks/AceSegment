d647be2c47beb74e94aa83e35e80c99b36b53b80
Brian Park <brian@xparks.net>
2021-03-28 08:25:17 -0700

    AceSegmentDemo.ino: Wrap Serial output with ENABLE_SERIAL_DEBUG

--- examples/AceSegmentDemo/AceSegmentDemo.ino
+++ examples/AceSegmentDemo/AceSegmentDemo.ino
@@ -7,6 +7,10 @@
   #include "FastSpiDriver.h"
 #endif
 
+#ifndef ENABLE_SERIAL_DEBUG
+#define ENABLE_SERIAL_DEBUG 0
+#endif
+
 using namespace ace_segment;
 using namespace ace_button;
 
@@ -39,14 +43,20 @@ void handleEvent(AceButton* /* button */, uint8_t eventType,
     case AceButton::kEventReleased:
       if (loopMode == LOOP_MODE_AUTO_RENDER) {
         loopMode = LOOP_MODE_PAUSE;
-        Serial.println(F("handleEvent(): paused"));
+        if (ENABLE_SERIAL_DEBUG >= 1) {
+          Serial.println(F("handleEvent(): paused"));
+        }
       } else {
-        Serial.println(F("handleEvent(): stepping"));
+        if (ENABLE_SERIAL_DEBUG >= 1) {
+          Serial.println(F("handleEvent(): stepping"));
+        }
         loopMode = LOOP_MODE_STEP;
       }
       break;
     case AceButton::kEventLongPressed:
-      Serial.println(F("handleEvent(): auto render"));
+      if (ENABLE_SERIAL_DEBUG >= 1) {
+        Serial.println(F("handleEvent(): auto render"));
+      }
       loopMode = LOOP_MODE_AUTO_RENDER;
       break;
   }
@@ -69,7 +79,7 @@ void setupAceButton() {
 #define DRIVER_MODE_DIRECT_DIGIT 1
 #define DRIVER_MODE_DIRECT_FAST_DIGIT 2
 #define DRIVER_MODE_DIRECT_SEGMENT 3
-#define DRIVER_MODE_SERIAL_DIGIT 4
+  #define DRIVER_MODE_SERIAL_DIGIT 4
 #define DRIVER_MODE_SERIAL_FAST_DIGIT 5
 #define DRIVER_MODE_SPI_DIGIT 6
 #define DRIVER_MODE_SPI_FAST_DIGIT 7
@@ -214,8 +224,10 @@ void setupAceSegment() {
   // set up Timer 2
   uint8_t timerCompareValue =
       (long) F_CPU / 1024 / renderer->getFieldsPerSecond() - 1;
-  Serial.print(F("Timer 2, Compare A: "));
-  Serial.println(timerCompareValue);
+  if (ENABLE_SERIAL_DEBUG >= 1) {
+    Serial.print(F("Timer 2, Compare A: "));
+    Serial.println(timerCompareValue);
+  }
 
   noInterrupts();
   TCNT2  = 0;	// Initialize counter value to 0
@@ -328,11 +340,13 @@ void autoRender() {
 #endif
 
   unsigned long now = millis();
-  if (now - lastUpdateTime > 200) {
+  if (now - lastUpdateTime > 250) {
     lastUpdateTime = now;
-    if (iter == 0) {
-      Serial.print(F("Demo Mode: "));
-      Serial.println(demoMode);
+    if (ENABLE_SERIAL_DEBUG >= 1) {
+      if (iter == 0) {
+        Serial.print(F("Demo Mode: "));
+        Serial.println(demoMode);
+      }
     }
 
     if (demoMode == DEMO_MODE_HEX) {
@@ -365,21 +379,23 @@ void autoRender() {
     uint16_t renderDurationMin = stats.getMin();
     uint16_t renderDurationMax = stats.getMax();
 
-    Serial.print(F("loops: "));
-    Serial.print(loopCount);
-    Serial.print(F("; renders: "));
-    Serial.print(elapsedCount);
-    Serial.print(F("; t: "));
-    Serial.print(elapsedTime / 1000);
-    Serial.print(F("s; fields/s: "));
-    Serial.print((uint32_t) elapsedCount * 1000 / elapsedTime);
-    Serial.print(F("Hz; min: "));
-    Serial.print(renderDurationMin);
-    Serial.print(F("us; avg: "));
-    Serial.print(renderDurationAverage);
-    Serial.print(F("us; max: "));
-    Serial.print(renderDurationMax);
-    Serial.println(F("us"));
+    if (ENABLE_SERIAL_DEBUG >= 1) {
+      Serial.print(F("loops: "));
+      Serial.print(loopCount);
+      Serial.print(F("; renders: "));
+      Serial.print(elapsedCount);
+      Serial.print(F("; t: "));
+      Serial.print(elapsedTime / 1000);
+      Serial.print(F("s; fields/s: "));
+      Serial.print((uint32_t) elapsedCount * 1000 / elapsedTime);
+      Serial.print(F("Hz; min: "));
+      Serial.print(renderDurationMin);
+      Serial.print(F("us; avg: "));
+      Serial.print(renderDurationAverage);
+      Serial.print(F("us; max: "));
+      Serial.print(renderDurationMax);
+      Serial.println(F("us"));
+    }
 
     stopWatchStart = now;
     loopCount = 0;
@@ -402,9 +418,11 @@ void setup() {
   delay(1000); // Wait for stability on some boards, otherwise garage on Serial
 #endif
 
-  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
-  while (!Serial); // Wait until Serial is ready - Leonardo/Micro
-  Serial.println(F("setup(): begin"));
+  if (ENABLE_SERIAL_DEBUG >= 1) {
+    Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+    while (!Serial); // Wait until Serial is ready - Leonardo/Micro
+    Serial.println(F("setup(): begin"));
+  }
 
   setupAceButton();
 
@@ -412,7 +430,9 @@ void setup() {
   setupAceSegment();
 #endif
 
-  Serial.println(F("setup(): end"));
+  if (ENABLE_SERIAL_DEBUG >= 1) {
+    Serial.println(F("setup(): end"));
+  }
 }
 
 void loop() {
