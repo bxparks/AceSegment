477f03eeb991bbc4016c15126b3f4a72383c4f5b
Brian Park <brian@xparks.net>
2021-03-30 19:35:03 -0700

    AutoBenchmark: Rewrite to support new SegmentDisplay variants

--- examples/AutoBenchmark/AutoBenchmark.ino
+++ examples/AutoBenchmark/AutoBenchmark.ino
@@ -24,157 +24,209 @@ SOFTWARE.
 
 /*
  * A sketch that generates the min/avg/max (in microsecondes) benchmarks of
- * Renderer::renderField() for all versions of the Driver which are supported
- * by AceSegment library. The output is an ASCII formatted table that can be
- * pasted directly into the README.md file as a code block. This saves a lot of
- * error-prone manual collection of these numbers.
+ * SegmentDisplay::renderField() for various configurations of LedMatrix. The
+ * output is an space-separate list of numbers which can be fed into
+ * `generate_table.awk` to extract a human-readable ASCII table that can be
+ * pasted directly into the README.md file as a code block.
  *
- * DriverConfig contains an enumeration of all Driver configurations
- * which are currently supported. For each DriverConfig, the Driver is
- * used to construct the Renderer stack and all of its dependencies. It calls
- * Renderer::displayField() a number of times (NUM_FIELD_SAMPLES is 1800), then
- * retrieves the TimingStats from the Renderer, and prints out the min/avg/max
- * numbers.
+ * Each DriverConfig configures the SegmentDisplay stack and all of its
+ * dependencies. It calls SegmentDisplay::displayField() a number of times
+ * (NUM_FIELD_SAMPLES is 1800), then retrieves the TimingStats from the
+ * SegmentDisplay, and prints out the min/avg/max numbers.
  */
 
 #include <stdio.h>
 #include <Arduino.h>
+#include <AceCommon.h> // TimingStats
 #include <AceSegment.h>
-#include "Flash.h"
-#ifdef ARDUINO_ARCH_AVR
-  #include "FastDirectDriver.h"
-  #include "FastSerialDriver.h"
-  #include "FastSpiDriver.h"
-#endif
-#include "DriverConfig.h"
-#include "BenchmarkBundle.h"
 
 using namespace ace_segment;
+using ace_common::TimingStats;
 
-void writeChars();
-void finishBenchmark();
-void setupBenchmark();
-void nextBenchmark();
-void printTimingStats(
-    const DriverConfig* driverConfig,
-    const TimingStats& stats);
-void printLabel(const DriverConfig* driverConfig);
+#if ! defined(SERIAL_PORT_MONITOR)
+#define SERIAL_PORT_MONITOR Serial
+#endif
 
 //------------------------------------------------------------------
-// Setup for AutoBenchmark
-//------------------------------------------------------------------
-const uint8_t LOOP_MODE_BEGIN = 0;
-const uint8_t LOOP_MODE_RENDER = 1;
-const uint8_t LOOP_MODE_NEXT_DRIVER = 2;
-const uint8_t LOOP_MODE_FOOTER = 3;
-const uint8_t LOOP_MODE_DONE = 4;
-static uint8_t loopMode = LOOP_MODE_BEGIN;
-
-const DriverConfig* driverConfig = nullptr;
-BenchmarkBundle* benchmarkBundle = nullptr;
-
-void finishBenchmark() {
-  if (benchmarkBundle == nullptr) return;
-
-  benchmarkBundle->finish();
-  delete benchmarkBundle;
-  benchmarkBundle = nullptr;
-}
-
-void setupBenchmark(const DriverConfig* driverConfig) {
-  benchmarkBundle = new BenchmarkBundle(driverConfig);
-  benchmarkBundle->configure();
-  CharWriter* writer = benchmarkBundle->mCharWriter;
-  writer->writeCharAt(0, '1');
-  writer->writeCharAt(1, '2');
-  writer->writeCharAt(2, '3');
-  writer->writeCharAt(3, '4');
-}
-
-//------------------------------------------------------------------
-// Loop for AutoBenchmark
+// Setup for AceSegment
 //------------------------------------------------------------------
 
-static const char kBoundary[] PROGMEM =
-    "-------------------------------|------------+--------+-------------+";
-static const char kHeader[] PROGMEM =
-    "driver                         | modulation | styles | min/avg/max |";
-static const char kDivider[] PROGMEM =
-    "-------------------------------|------------|--------|-------------|";
+const uint8_t FRAMES_PER_SECOND = 60;
+const uint8_t NUM_SUBFIELDS = 1;
 
-void render() {
-  // Number of renderFields() to sample, should be between (1, 2) *
-  // RenderBuilder::kStatsResetIntervalDefault so that we have one reset of
-  // TimingStats to avoid spurious times in the initial iterations. For 4
-  // digits, with 16 subfields/field, there are 64 fields per frame. So 1800
-  // fields means 28 frames, which means about 1/2 second at 60 frames per
-  // second.
-  const uint16_t NUM_FIELD_SAMPLES = 1800;
+const uint8_t NUM_DIGITS = 4;
+const uint8_t NUM_SEGMENTS = 8;
 
-  bool isRendered = benchmarkBundle->mRenderer->renderFieldWhenReady();
+#if defined(ARDUINO_ARCH_AVR)
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {4, 5, 6, 7};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {8, 9, 10, 16, 14, 18, 19, 15};
+#elif defined(ARDUINO_ARCH_SAMD)
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {2, 3, 4, 5};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {6, 7, 8, 9, 10, 11, 12, 13};
+#elif defined(ARDUINO_ARCH_STM32)
+  // I think this is the F1, because there exists a ARDUINO_ARCH_STM32F4
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {2, 3, 4, 5};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {6, 7, 8, 9, 10, 11, 12, 13};
+#elif defined(ESP8266)
+  // Don't have enough pins so reuse some.
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {D4, D5, D4, D5};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {D6, D7, D6, D7, D6, D7, D6, D7};
+#elif defined(ESP32)
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {21, 22, 23, 24};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {12, 13, 14, 15, 16, 17, 18, 19};
+#elif defined(TEENSYDUINO)
+  // Teensy 3.2
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {2, 3, 4, 5};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {6, 7, 8, 9, 10, 11, 12, 13};
+#else
+  #warning Unknown hardware, using some defaults
+  const uint8_t DIGIT_PINS[NUM_DIGITS] = {2, 3, 4, 5};
+  const uint8_t SEGMENT_PINS[NUM_SEGMENTS] = {2, 3, 4, 5, 6, 7, 8, 9};
+#endif
 
-  if (isRendered) {
-    uint16_t elapsedCount = benchmarkBundle->mCurrentStatsCounter -
-        benchmarkBundle->mLastStatsCounter;
-    if (elapsedCount >= NUM_FIELD_SAMPLES) {
-      TimingStats stats = benchmarkBundle->mRenderer->getTimingStats();
-      printTimingStats(driverConfig, stats);
-      loopMode = LOOP_MODE_NEXT_DRIVER;
-    } else {
-      benchmarkBundle->mCurrentStatsCounter++;
-    }
-  }
+const uint8_t LATCH_PIN = 10; // ST_CP on 74HC595
+const uint8_t DATA_PIN = MOSI; // DS on 74HC595
+const uint8_t CLOCK_PIN = SCK; // SH_CP on 74HC595
+
+Hardware hardware;
+
+// Common Anode, with transistors on Group pins
+LedMatrixDirect ledMatrixDirect(
+    &hardware,
+    LedMatrix::kActiveLowPattern /*groupOnPattern*/,
+    LedMatrix::kActiveLowPattern /*elementOnPattern*/,
+    NUM_DIGITS,
+    DIGIT_PINS,
+    NUM_SEGMENTS,
+    SEGMENT_PINS);
+
+// Common Cathode, with transistors on Group pins
+SwSpiAdapter swSpiAdapter(LATCH_PIN, DATA_PIN, CLOCK_PIN);
+LedMatrixPartialSpi ledMatrixPartialSwSpi(
+    &hardware,
+    &swSpiAdapter,
+    LedMatrix::kActiveHighPattern /*groupOnPattern*/,
+    LedMatrix::kActiveHighPattern /*elementOnPattern*/,
+    NUM_DIGITS,
+    DIGIT_PINS);
+
+// Common Cathode, with transistors on Group pins
+HwSpiAdapter hwSpiAdapter(LATCH_PIN, DATA_PIN, CLOCK_PIN);
+LedMatrixPartialSpi ledMatrixPartialHwSpi(
+    &hardware,
+    &hwSpiAdapter,
+    LedMatrix::kActiveHighPattern /*groupOnPattern*/,
+    LedMatrix::kActiveHighPattern /*elementOnPattern*/,
+    NUM_DIGITS,
+    DIGIT_PINS);
+
+// Common Anode, with transistors on Group pins
+LedMatrixFullSpi ledMatrixFullSwSpi(
+    &swSpiAdapter,
+    LedMatrix::kActiveLowPattern /*groupOnPattern*/,
+    LedMatrix::kActiveLowPattern /*elementOnPattern*/);
+
+// Common Anode, with transistors on Group pins
+LedMatrixFullSpi ledMatrixFullHwSpi(
+    &hwSpiAdapter,
+    LedMatrix::kActiveLowPattern /*groupOnPattern*/,
+    LedMatrix::kActiveLowPattern /*elementOnPattern*/);
+
+uint8_t patterns[NUM_DIGITS];
+
+//------------------------------------------------------------------
+// Run benchmarks.
+//------------------------------------------------------------------
+
+// Each frame has NUM_DIGITS * NUM_SUBFIELDS fields. At 60 frames/second and 4
+// digits, and NUM_FIELD_SAMPLES=1, we want at least 240 samples. Let's grab
+// about 1/2 second worth.
+const uint16_t NUM_FIELD_SAMPLES = 1200;
+
+/** Print the result for each LedMatrix algorithm. */
+static void printStats(const char* name, TimingStats& stats) {
+  SERIAL_PORT_MONITOR.print(name);
+  SERIAL_PORT_MONITOR.print(' ');
+  SERIAL_PORT_MONITOR.print(stats.getMin());
+  SERIAL_PORT_MONITOR.print(' ');
+  SERIAL_PORT_MONITOR.print(stats.getAvg());
+  SERIAL_PORT_MONITOR.print(' ');
+  SERIAL_PORT_MONITOR.print(stats.getMax());
+  SERIAL_PORT_MONITOR.print(' ');
+  SERIAL_PORT_MONITOR.println(NUM_FIELD_SAMPLES);
 }
 
-void nextBenchmark() {
-  driverConfig++;
-  if (driverConfig >=
-      &DriverConfig::kDriverConfigs[0] + DriverConfig::kNumDriverConfigs) {
-    loopMode = LOOP_MODE_FOOTER;
-  } else {
-    setupBenchmark(driverConfig);
-    loopMode = LOOP_MODE_RENDER;
+void runBenchmark(const char* name, LedMatrix& ledMatrix) {
+  TimingStats timingStats;
+  ledMatrix.begin();
+
+  Renderer renderer(&ledMatrix, NUM_DIGITS, patterns);
+  renderer.begin();
+
+  SegmentDisplay segmentDisplay(
+      &hardware,
+      &renderer,
+      FRAMES_PER_SECOND,
+      NUM_DIGITS,
+      patterns,
+      nullptr /*brightnesses*/,
+      &timingStats);
+  segmentDisplay.begin();
+  segmentDisplay.writePatternAt(0, 0x13);
+  segmentDisplay.writePatternAt(0, 0x37);
+  segmentDisplay.writePatternAt(0, 0x7F);
+  segmentDisplay.writePatternAt(0, 0xFF);
+
+  for (uint16_t i = 0; i < NUM_FIELD_SAMPLES; i++) {
+    segmentDisplay.renderField();
+    yield();
   }
+
+  segmentDisplay.end();
+  renderer.end();
+  ledMatrix.end();
+
+  printStats(name, timingStats);
+  timingStats.reset();
 }
 
-void printTimingStats(const DriverConfig* driverConfig,
-    const TimingStats& stats) {
-  printLabel(driverConfig);
+void runBenchmarks() {
+  SERIAL_PORT_MONITOR.println(F("BENCHMARKS"));
 
-  char buf[15]; // 12 should be enough, but give 3 more just in case
-  sprintf(buf, " %3d/%3d/%3d |", stats.getMin(), stats.getAvg(),
-      stats.getMax());
-  Serial.println(buf);
+  runBenchmark("direct", ledMatrixDirect);
+  runBenchmark("partial_sw_spi", ledMatrixPartialSwSpi);
+  runBenchmark("partial_hw_spi", ledMatrixPartialHwSpi);
+  runBenchmark("full_sw_spi", ledMatrixFullSwSpi);
+  runBenchmark("full_hw_spi", ledMatrixFullHwSpi);
 }
 
-void printLabel(const DriverConfig* driverConfig) {
-  // Print driver label
-  uint8_t index = static_cast<uint8_t>(driverConfig->mDriverOption);
-  const char* label = DriverConfig::kDriverOptionLabels[index];
-  uint8_t width = strlen_P(label);
-  // maxWidth could be calculated just once, but good enough for now
-  uint8_t maxWidth = DriverConfig::getMaxWidthDriverLabel();
-  Serial.print(FPSTR(label));
-  for (uint8_t i = 0; i < maxWidth - width; i++) {
-    Serial.print(' ');
-  }
-  Serial.print(' ');
+void printSizeOf() {
+  SERIAL_PORT_MONITOR.println(F("SIZEOF"));
 
-  // Print modulation option
-  if (driverConfig->mModulation == DriverConfig::NoModulation) {
-    Serial.print("| -          ");
-  } else {
-    Serial.print("| Y          ");
-  }
-
-  // Print style option
-  if (driverConfig->mStyle == DriverConfig::NoStyles) {
-    Serial.print("| -      ");
-  } else {
-    Serial.print("| Y      ");
-  }
-
-  Serial.print("|");
+  SERIAL_PORT_MONITOR.print(F("sizeof(Hardware): "));
+  SERIAL_PORT_MONITOR.println(sizeof(Hardware));
+  SERIAL_PORT_MONITOR.print(F("sizeof(SwSpiAdapter): "));
+  SERIAL_PORT_MONITOR.println(sizeof(SwSpiAdapter));
+  SERIAL_PORT_MONITOR.print(F("sizeof(HwSpiAdapter): "));
+  SERIAL_PORT_MONITOR.println(sizeof(HwSpiAdapter));
+  SERIAL_PORT_MONITOR.print(F("sizeof(LedMatrixDirect): "));
+  SERIAL_PORT_MONITOR.println(sizeof(LedMatrixDirect));
+  SERIAL_PORT_MONITOR.print(F("sizeof(LedMatrixPartialSpi): "));
+  SERIAL_PORT_MONITOR.println(sizeof(LedMatrixPartialSpi));
+  SERIAL_PORT_MONITOR.print(F("sizeof(LedMatrixFullSpi): "));
+  SERIAL_PORT_MONITOR.println(sizeof(LedMatrixFullSpi));
+  SERIAL_PORT_MONITOR.print(F("sizeof(Renderer): "));
+  SERIAL_PORT_MONITOR.println(sizeof(Renderer));
+  SERIAL_PORT_MONITOR.print(F("sizeof(SegmentDisplay): "));
+  SERIAL_PORT_MONITOR.println(sizeof(SegmentDisplay));
+  SERIAL_PORT_MONITOR.print(F("sizeof(HexWriter): "));
+  SERIAL_PORT_MONITOR.println(sizeof(HexWriter));
+  SERIAL_PORT_MONITOR.print(F("sizeof(ClockWriter): "));
+  SERIAL_PORT_MONITOR.println(sizeof(ClockWriter));
+  SERIAL_PORT_MONITOR.print(F("sizeof(CharWriter): "));
+  SERIAL_PORT_MONITOR.println(sizeof(CharWriter));
+  SERIAL_PORT_MONITOR.print(F("sizeof(StringWriter): "));
+  SERIAL_PORT_MONITOR.println(sizeof(StringWriter));
 }
 
 //-----------------------------------------------------------------------------
@@ -184,29 +236,16 @@ void setup() {
   delay(1000); // Wait for stability on some boards, otherwise garage on Serial
 #endif
 
-  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
-  while (!Serial); // Wait until Serial is ready - Leonardo/Micro
-  Serial.println(F("setup(): begin"));
+  SERIAL_PORT_MONITOR.begin(115200);
+  while (!SERIAL_PORT_MONITOR); // Wait for Leonardo/Micro
 
-  driverConfig = &DriverConfig::kDriverConfigs[0];
-  setupBenchmark(driverConfig);
+  printSizeOf();
+  runBenchmarks();
+  SERIAL_PORT_MONITOR.println("END");
 
-  Serial.println(F("setup(): end"));
+#if defined(EPOXY_DUINO)
+  exit(0);
+#endif
 }
 
-void loop() {
-  if (loopMode == LOOP_MODE_BEGIN) {
-    Serial.println(FPSTR(kBoundary));
-    Serial.println(FPSTR(kHeader));
-    Serial.println(FPSTR(kDivider));
-    loopMode = LOOP_MODE_RENDER;
-  } else if (loopMode == LOOP_MODE_RENDER) {
-    render();
-  } else if (loopMode == LOOP_MODE_NEXT_DRIVER) {
-    finishBenchmark();
-    nextBenchmark();
-  } else if (loopMode == LOOP_MODE_FOOTER) {
-    Serial.println(FPSTR(kBoundary));
-    loopMode = LOOP_MODE_DONE;
-  }
-}
+void loop() {}
