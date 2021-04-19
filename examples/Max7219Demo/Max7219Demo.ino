4bf53ad7d3a3cfc29c700e29408652984b7bcee9
Brian Park <brian@xparks.net>
2021-04-19 12:45:00 -0700

    src/max7219: Add first working implementation of Max7219Module class

---
+++ examples/Max7219Demo/Max7219Demo.ino
@@ -0,0 +1,110 @@
+/*
+ * A simple demo of a single, 8-digit LED module using a MAX7219 chip.
+ */
+
+#include <Arduino.h>
+#include <AceCommon.h> // incrementMod()
+#include <AceSegment.h> // Max7219Module, LedDisplay
+
+using ace_common::incrementMod;
+using ace_common::incrementModOffset;
+using ace_common::TimingStats;
+using ace_segment::Max7219Module;
+using ace_segment::LedDisplay;
+using ace_segment::HwSpiInterface;
+using ace_segment::SwSpiInterface;
+using ace_segment::kEightDigitRemapArray;
+
+// Select SPI interface type.
+#define SPI_INTERFACE_TYPE_HW 0
+#define SPI_INTERFACE_TYPE_SW 1
+#define SPI_INTERFACE_TYPE_SW_FAST 2
+#define SPI_INTERFACE_TYPE SPI_INTERFACE_TYPE_SW_FAST
+
+#if SPI_INTERFACE_TYPE == SPI_INTERFACE_TYPE_SW_FAST
+  #include <digitalWriteFast.h>
+  #include <ace_segment/hw/SwSpiFastInterface.h>
+  using ace_segment::SwSpiFastInterface;
+#endif
+
+// SPI pins
+const uint8_t LATCH_PIN = A0;
+const uint8_t DATA_PIN = MOSI;
+const uint8_t CLOCK_PIN = SCK;
+
+// LED segment patterns.
+const uint8_t NUM_DIGITS = 8;
+const uint8_t PATTERNS[NUM_DIGITS] = {
+  0b00111111, // 0
+  0b00000110, // 1
+  0b01011011, // 2
+  0b01001111, // 3
+  0b01100110, // 4
+  0b01101101, // 5
+  0b01111101, // 6
+  0b00000111, // 7
+};
+
+#if SPI_INTERFACE_TYPE == SPI_INTERFACE_TYPE_HW
+  using SpiInterface = HwSpiInterface;
+  SpiInterface spiInterface(LATCH_PIN, DATA_PIN, CLOCK_PIN);
+#elif SPI_INTERFACE_TYPE == SPI_INTERFACE_TYPE_SW
+  using SpiInterface = SwSpiInterface;
+  SpiInterface spiInterface(LATCH_PIN, DATA_PIN, CLOCK_PIN);
+#elif SPI_INTERFACE_TYPE == SPI_INTERFACE_TYPE_SW_FAST
+  using SpiInterface = SwSpiFastInterface<LATCH_PIN, DATA_PIN, CLOCK_PIN>;
+  SpiInterface spiInterface;
+#else
+  #error Unknown SPI_INTERFACE_TYPE
+#endif
+
+Max7219Module<SpiInterface, NUM_DIGITS> max7219Module(
+    spiInterface, kEightDigitRemapArray);
+LedDisplay display(max7219Module);
+
+TimingStats stats;
+
+uint8_t digitIndex = 0;
+uint8_t brightness = 1;
+
+void setup() {
+  delay(1000);
+#if ENABLE_SERIAL_DEBUG >= 1
+  Serial.begin(115200);
+  while (!Serial);
+#endif
+
+  spiInterface.begin();
+  max7219Module.begin();
+}
+
+// Use the Max7219Module::flush() method to update all digits in a single dump
+// to the LED module, taking about ~170 microseconds per flush().
+void loop() {
+  // Update the display
+  uint8_t j = digitIndex;
+  for (uint8_t i = 0; i < NUM_DIGITS; ++i) {
+    display.writePatternAt(i, PATTERNS[j]);
+    // Write a decimal point every other digit, for demo purposes.
+    display.writeDecimalPointAt(i, j & 0x1);
+    incrementMod(j, (uint8_t) NUM_DIGITS);
+  }
+  incrementMod(digitIndex, (uint8_t) NUM_DIGITS);
+
+  // Update the brightness
+  display.setBrightness(brightness);
+  incrementMod(brightness, (uint8_t) 16);
+
+  // Flush the change to the LED Module, and measure the time.
+  uint16_t startMicros = micros();
+  max7219Module.flush();
+  uint16_t elapsedMicros = (uint16_t) micros() - startMicros;
+  stats.update(elapsedMicros);
+
+#if ENABLE_SERIAL_DEBUG >= 1
+  Serial.print("ExpAvg:");
+  Serial.println(stats.getExpDecayAvg());
+#endif
+
+  delay(1000);
+}
---
+++ src/ace_segment/max7219/Max7219Module.h
@@ -0,0 +1,183 @@
+/*
+MIT License
+
+Copyright (c) 2021 Brian T. Park
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef ACE_SEGMENT_MAX7219_MODULE_H
+#define ACE_SEGMENT_MAX7219_MODULE_H
+
+#include <stdint.h>
+#include <string.h> // memset()
+#include "../LedModule.h"
+
+namespace ace_segment {
+
+/**
+ * The 8-digit MAX7219 LED modules that I bought on eBay and Amazon have
+ * the digits wired so that digit 0 is the *right* most digit, and digit 7 is
+ * the *left* most digit. This is the reverse of the convention used by the
+ * `LedDisplay` class. Use this remap array to reverse the digit addresses.
+ *
+ * You can create your own remap array to handle other LED modules with
+ * different physical ordering compared to the logical ordering.
+ *
+ * Pass this array into the Max7219Module constructor.
+ */
+static const uint8_t kEightDigitRemapArray[8] = {
+  7, 6, 5, 4, 3, 2, 1, 0
+};
+
+template <typename SPII, uint8_t DIGITS>
+class Max7219Module : public LedModule {
+  public:
+    /** Constructor */
+    Max7219Module(
+        const SPII& spiInterface,
+        const uint8_t* remapArray = nullptr
+    ) :
+        LedModule(DIGITS),
+        mSpiInterface(spiInterface),
+        mRemapArray(remapArray)
+    {}
+
+    //-----------------------------------------------------------------------
+    // Initialization and termination.
+    //-----------------------------------------------------------------------
+
+    void begin() {
+      memset(mPatterns, 0, DIGITS);
+
+      mSpiInterface.send16(kRegisterScanLimit, 7); // all digits
+      mSpiInterface.send16(kRegisterDecodeMode, 0); // no BCD decoding
+      mSpiInterface.send16(kRegisterShutdown, 0x1); // turn on
+    }
+
+    void end() {
+      mSpiInterface.send16(kRegisterShutdown, 0x0); // turn off
+    }
+
+    //-----------------------------------------------------------------------
+    // Implement the LedModule interface
+    //-----------------------------------------------------------------------
+
+    /** Return the number of digits supported by this display instance. */
+    uint8_t getNumDigits() const { return DIGITS; }
+
+    void setPatternAt(uint8_t pos, uint8_t pattern) override {
+      mPatterns[pos] = pattern;
+    }
+
+    uint8_t getPatternAt(uint8_t pos) override {
+      return mPatterns[pos];
+    }
+
+    void setBrightness(uint8_t brightness) override {
+      mBrightness = brightness & 0xF;
+    }
+
+    //-----------------------------------------------------------------------
+    // Methods related to rendering.
+    //-----------------------------------------------------------------------
+
+    /**
+     * Send segment patterns of all digits. For a rough idea of how long
+     * this function takes, here are the numbers on a 16 MHz AVR:
+     *
+     *  * HW SPI: 170 microseconds
+     *  * SW SPI: 1800 microseconds
+     *  * SW SPI Fast: 210 microseconds
+     */
+    void flush() {
+      for (uint8_t i = 0; i < DIGITS; ++i) {
+        uint8_t actualPos = remapDigit(i);
+        uint8_t convertedPattern = convertPattern(mPatterns[i]);
+        mSpiInterface.send16(actualPos + 1, convertedPattern);
+      }
+
+      mSpiInterface.send16(kRegisterIntensity, mBrightness);
+    }
+
+  private:
+    /**
+     * MAX7219 uses bit 0 for segment G, and bit 6 for segment A. This is
+     * the reverse of what I would normally expect, certainly the reverse of the
+     * TM1637. The weird thing is that the MAX7219 still uses bit 7 for the
+     * decimal point. This method converts the normalized pattern into the
+     * pattern expected by the MAX7219.
+     */
+    static uint8_t convertPattern(uint8_t pattern) {
+      uint8_t result = 0;
+
+      // Reverse the first 7 bits.
+      for (uint8_t i = 0; i < 7; ++i) {
+        result <<= 1;
+        if (pattern & 0x1) {
+          result |= 0x1;
+        }
+        pattern >>= 1;
+      }
+
+      // Transfer the decimal point on bit 8.
+      if (pattern & 0x1) {
+        result |= 0x80;
+      }
+
+      return result;
+    }
+
+    /** Convert a logical position into the physical position. */
+    uint8_t remapDigit(uint8_t pos) const {
+      return mRemapArray ? mRemapArray[pos] : pos;
+    }
+
+  private:
+    static uint8_t const kRegisterNoop        = 0x00;
+    static uint8_t const kRegisterDigit0      = 0x01;
+    static uint8_t const kRegisterDigit1      = 0x02;
+    static uint8_t const kRegisterDigit2      = 0x03;
+    static uint8_t const kRegisterDigit3      = 0x04;
+    static uint8_t const kRegisterDigit4      = 0x05;
+    static uint8_t const kRegisterDigit5      = 0x06;
+    static uint8_t const kRegisterDigit6      = 0x07;
+    static uint8_t const kRegisterDigit7      = 0x08;
+    static uint8_t const kRegisterDecodeMode  = 0x09;
+    static uint8_t const kRegisterIntensity   = 0x0A;
+    static uint8_t const kRegisterScanLimit   = 0x0B;
+    static uint8_t const kRegisterShutdown    = 0x0C;
+    static uint8_t const kRegisterDisplayTest = 0x0F;
+
+    /** SPI interface. */
+    const SPII& mSpiInterface;
+
+    /** Array to map digit addresses. */
+    const uint8_t* const mRemapArray;
+
+    /** Pattern for each digit. */
+    uint8_t mPatterns[DIGITS];
+
+    /** Brightness 0 - 15 */
+    uint8_t mBrightness;
+};
+
+}
+
+#endif
