bxparks/AceSegment

commit 0c37b29a29caf0d8132d2ab4b02adee2e78763b6

src/{Tm1637,Max7219,DualHc595}Module: Clean up remapPhysicalToLogical() logic; add invertRemapArray() to DualHc595Module because LedMatrixDualHc595 requires a remapLogicalToPhysical() function

src/ace_segment/tm1637/Tm1637Module.h
@@ -42,23 +42,41 @@ namespace ace_segment {
 static const uint16_t kDefaultTm1637DelayMicros = 100;
 
 /**
+ * A map of the physical digit position to its logical position. In other words
+ * `logicalPos = kDigitRemapArray[physicalPos]`. Pass this array into the
+ * Tm1637Module constructor.
+ *
  * Many (if not all) of the 6-digit LED modules on eBay and Amazon using the
- * TM1637 chip are wired out of sequence. Not sure why since their 4-digit LED
- * modules follow the natural order. This array remaps the digit position to the
- * correct order expected by this library where digit 0 is on the left, and
- * digit 5 is on the far right. Pass this array into the Tm1637Module
- * constructor.
+ * TM1637 chip are wired so that the digits appear in the order of "2 1 0 5 4 3"
+ * instead of "0 1 2 3 4 5". Not sure why since the 4-digit LED modules seem to
+ * follow the natural order.
  *
  * You can create your own remap array to handle other LED modules with
  * different physical ordering compared to the logical ordering.
  */
 extern const uint8_t kDigitRemapArray6Tm1637[6];
 
+namespace internal {
+
 /**
- * An implementation of seven-segment LedModule using the TM1637 chip. The chip
- * communicates using a protocol that is electrically similar to I2C, but does
- * not use an address byte at the beginning of the protocol. We can use a
- * software-based I2C interface.
+ * Return a bit mask that sets the lower `numBits` bits to 1. For example, if
+ * `numBits=4`, then we want 0b00001111 or 0x0F. The general expression to get
+ * this result is `2^N - 1`. The exponential operation can be performed using a
+ * left bit-shift. I believe this expression is well-defined in the C++ language
+ * even if `numBits == 8` because `(1 << 8)` is 0, and `0 - 1` is `0xFF` using
+ * unsigned integer arithmetics.
+ */
+constexpr uint8_t initialDirtyBits(uint8_t numBits) {
+  return ((uint8_t) 0x1 << numBits) - 1;
+}
+
+} // namespace internal
+
+/**
+ * An implementation of LedModule using the TM1637 chip. The chip communicates
+ * using a protocol that is electrically similar to I2C, but does not use an
+ * address byte at the beginning of the protocol. We can use a software-based
+ * I2C interface.
  *
  * @tparam T_WI wire protocol interface, either SoftWireInterface or
  *    SoftWireFastInterface
@@ -72,8 +90,9 @@ class Tm1637Module : public LedModule {
      * Constructor.
      * @param wireInterface instance of either SoftWireInterface or
      *    SoftWireFastInterface
-     * @param remapArray (optional) some (most?) six-digit LED modules using the
-     *      TM1637 chip need remapping of the digit addresses
+     * @param remapArray (optional, nullable) a mapping of the physical digit
+     *    positions to their logical positions, useful for 6-digt LED modules
+     *    using the TM1637 chip whose digits are wired out of order
      */
     explicit Tm1637Module(
         const T_WI& wireInterface,
@@ -100,7 +119,13 @@ class Tm1637Module : public LedModule {
     void begin() {
       memset(mPatterns, 0, T_DIGITS);
       mBrightness = kBrightnessCmd | kBrightnessLevelOn | 0x7;
-      mIsDirty = 0xFF; // force initial values to LED module
+
+      // Initially, we want to set all the dirty bits for digits and brightness,
+      // so that they are sent to the LED module upon the first flush() or
+      // flushIncremental(). The number of dirty bits required is `T_DIGITS +
+      // 1` because we use an extra bit for the brightness.
+      mIsDirty = internal::initialDirtyBits(T_DIGITS + 1);
+
       mFlushStage = 0;
     }
 
@@ -150,25 +175,28 @@ class Tm1637Module : public LedModule {
     //-----------------------------------------------------------------------
 
     /**
-     * Send segment patterns of all digits, plus the brightness information to
-     * the display. Takes about 22 ms using a 100 microsecond delay.
+     * Send segment patterns of all digits plus the brightness to the display,
+     * if any of the digits or brightness is dirty. Takes about 22 ms using a
+     * 100 microsecond delay.
      */
     void flush() {
+      if (! mIsDirty) return;
+
       // Update the brightness first
       mWireInterface.startCondition();
       mWireInterface.sendByte(mBrightness);
       mWireInterface.stopCondition();
 
-      // Update the digits.
+      // Update the digits using auto incrementing mode.
       mWireInterface.startCondition();
       mWireInterface.sendByte(kDataCmdAutoAddress);
       mWireInterface.stopCondition();
 
       mWireInterface.startCondition();
       mWireInterface.sendByte(kAddressCmd);
-      for (uint8_t i = 0; i < T_DIGITS; ++i) {
-        uint8_t actualPos = remapDigit(i);
-        mWireInterface.sendByte(mPatterns[actualPos]);
+      for (uint8_t physicalPos = 0; physicalPos < T_DIGITS; ++physicalPos) {
+        uint8_t logicalPos = remapPhysicalToLogical(physicalPos);
+        mWireInterface.sendByte(mPatterns[logicalPos]);
       }
       mWireInterface.stopCondition();
 
@@ -203,27 +231,31 @@ class Tm1637Module : public LedModule {
      * method is about 50% slower (30 ms), compared to flush() (22 ms).
      */
     void flushIncremental() {
-      if (isDirtyBit(mFlushStage)) {
-        if (mFlushStage == T_DIGITS) {
-          // Check for brightness change.
-          mWireInterface.startCondition();
-          mWireInterface.sendByte(mBrightness);
-          mWireInterface.stopCondition();
-        } else {
-          // Check for changed digits.
-          mWireInterface.startCondition();
-          mWireInterface.sendByte(kDataCmdFixedAddress);
-          mWireInterface.stopCondition();
+      if (mFlushStage == T_DIGITS) {
+        // Update brightness.
+        if (! isDirtyBit(T_DIGITS)) return;
+        mWireInterface.startCondition();
+        mWireInterface.sendByte(mBrightness);
+        mWireInterface.stopCondition();
+        clearDirtyBit(T_DIGITS);
+      } else {
+        const uint8_t physicalPos = mFlushStage;
+        const uint8_t logicalPos = remapPhysicalToLogical(physicalPos);
+        if (! isDirtyBit(logicalPos)) return;
 
-          mWireInterface.startCondition();
-          uint8_t actualPos = remapDigit(mFlushStage);
-          mWireInterface.sendByte(kAddressCmd | actualPos);
-          mWireInterface.sendByte(mPatterns[actualPos]);
-          mWireInterface.stopCondition();
-        }
-        clearDirtyBit(mFlushStage);
+        // Update changed digit.
+        mWireInterface.startCondition();
+        mWireInterface.sendByte(kDataCmdFixedAddress);
+        mWireInterface.stopCondition();
+
+        mWireInterface.startCondition();
+        mWireInterface.sendByte(kAddressCmd | physicalPos);
+        mWireInterface.sendByte(mPatterns[logicalPos]);
+        mWireInterface.stopCondition();
+        clearDirtyBit(logicalPos);
       }
 
+      // An extra dirty bit is used for the brightness so use `T_DIGITS + 1`.
       ace_common::incrementMod(mFlushStage, (uint8_t) (T_DIGITS + 1));
     }
 
@@ -240,8 +272,8 @@ class Tm1637Module : public LedModule {
       return mIsDirty & (0x1 << bit);
     }
 
-    /** Convert a logical position into the physical position. */
-    uint8_t remapDigit(uint8_t pos) const {
+    /** Convert a physical position into the logical position. */
+    uint8_t remapPhysicalToLogical(uint8_t pos) const {
       return mRemapArray ? mRemapArray[pos] : pos;
     }
 
@@ -262,8 +294,8 @@ class Tm1637Module : public LedModule {
     // 32-bit processors.
     const T_WI& mWireInterface;
     const uint8_t* const mRemapArray;
-    uint8_t mPatterns[T_DIGITS]; // maps to dirty bits 0-5
-    uint8_t mBrightness; // maps to dirty bit 7
+    uint8_t mPatterns[T_DIGITS]; // maps to dirty bits [0, T_DIGITS-1]
+    uint8_t mBrightness; // maps to dirty bit at T_DIGITS
     uint8_t mIsDirty; // bit array
     uint8_t mFlushStage; // [0, T_DIGITS], with T_DIGITS for brightness update
 };

tests/Tm1637ModuleTest/Tm1637ModuleTest.ino
@@ -1,0 +1,41 @@
+#line 2 "Tm1637ModuleTest.ino"
+
+/*
+ * MIT License
+ * Copyright (c) 2021 Brian T. Park
+ */
+
+#include <stdarg.h>
+#include <Arduino.h>
+#include <AUnitVerbose.h>
+#include <AceSegment.h>
+#include <ace_segment/testing/TestableSpiInterface.h>
+
+using aunit::TestRunner;
+using ace_segment::internal::initialDirtyBits;
+
+//----------------------------------------------------------------------------
+
+test(Tm1637ModuleTest, initialDirtyBits) {
+  assertEqual(0b0, initialDirtyBits(0));
+  assertEqual(0b1, initialDirtyBits(1));
+  assertEqual(0b11, initialDirtyBits(2));
+  assertEqual(0b111, initialDirtyBits(3));
+  assertEqual(0b1111111, initialDirtyBits(7));
+  assertEqual(0b11111111, initialDirtyBits(8));
+}
+
+//----------------------------------------------------------------------------
+
+void setup() {
+#if ! defined(EPOXY_DUINO)
+  delay(1000); // Wait for stability on some boards, otherwise garage on Serial
+#endif
+
+  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+  while (!Serial); // Wait until Serial is ready - Leonardo/Micro
+}
+
+void loop() {
+  TestRunner::run();
+}

examples/Max7219Demo/Max7219Demo.ino
@@ -26,7 +26,7 @@ using ace_segment::kDigitRemapArray8Max7219;
   #define SPI_INTERFACE_TYPE SPI_INTERFACE_TYPE_HARD_SPI_FAST
 
   // SPI pins
-  const uint8_t LATCH_PIN = A0;
+  const uint8_t LATCH_PIN = 10;
   const uint8_t DATA_PIN = MOSI;
   const uint8_t CLOCK_PIN = SCK;
 
@@ -34,7 +34,7 @@ using ace_segment::kDigitRemapArray8Max7219;
   #define SPI_INTERFACE_TYPE SPI_INTERFACE_TYPE_HARD_SPI_FAST
 
   // SPI pins
-  const uint8_t LATCH_PIN = A0;
+  const uint8_t LATCH_PIN = 10;
   const uint8_t DATA_PIN = MOSI;
   const uint8_t CLOCK_PIN = SCK;
 

examples/Tm1637Demo/Tm1637Demo.ino
@@ -51,7 +51,7 @@ const uint8_t PATTERNS[6] = {
 
 #elif defined(AUNITER_MICRO_TM1637_6)
   #define WIRE_INTERFACE_TYPE WIRE_INTERFACE_TYPE_FAST
-  #define TM_FLUSH_METHOD TM_FLUSH_METHOD_NORMAL
+  #define TM_FLUSH_METHOD TM_FLUSH_METHOD_INCREMENTAL
 
   const uint8_t CLK_PIN = A0;
   const uint8_t DIO_PIN = 9;

src/ace_segment/hc595/DualHc595Module.h
@@ -37,14 +37,37 @@ const uint8_t kByteOrderDigitHighSegmentLow = kByteOrderGroupHighElementLow;
 const uint8_t kByteOrderSegmentHighDigitLow = kByteOrderElementHighGroupLow;
 
 /**
+ * A map of the physical digit position to its physical position. In other words
+ * `logicalPos = kDigitRemapArray[physicalPos]`. Pass this array into the
+ * DualHc595Module constructor.
+ *
  * The 8-digit LED module from diymore.cc using dual 74HC595 controller chips
- * are wired such that the left 4-digits and right 4-digits are flipped.
- * This remap array fixes that.
+ * are wired such that the digits appear as "4 5 6 7 0 1 2 3" instead of "0 1 2
+ * 3 4 5 6 7". This remap array fixes that.
  *
  * You can create your own remap array to handle other LED modules with
  * different physical ordering compared to the logical ordering.
  */
 extern const uint8_t kDigitRemapArray8Hc595[8];
+
+namespace internal {
+
+/**
+ * Convert the physical-to-logical array to the logical-to-physical array
+ * required by LedMatrixDualHc595.
+ */
+static void invertRemapArray(
+    uint8_t logicalToPhysical[],
+    const uint8_t physicalToLogical[],
+    uint8_t numDigits
+) {
+  for (uint8_t physicalPos = 0; physicalPos < numDigits; ++physicalPos) {
+    uint8_t logicalPos = physicalToLogical[physicalPos];
+    logicalToPhysical[logicalPos] = physicalPos;
+  }
+}
+
+} // namespace internal
 
 /**
  * An implementation of LedModule class that supports an LED module using 2
@@ -93,10 +116,10 @@ class DualHc595Module : public ScanningModule<
      * @param byteOrder whether to send the digit patterns first
      *    (kByteOrderDigitHighSegmentLow) or segment patterns first
      *    (kByteOrderSegmentHighDigitLow)
-     * @param remapArray (optional) some LED modules using the 74HC595 chip need
-     *    their physical digit positions remapped to their logical positions
-     *    (e.g. the 8-digit LED modules from diymore.cc have the left 4 and
-     *    right 4 LED digits swapped)
+     * @param remapArray (optional, nullable) a mapping from the physical digit
+     *    positions to their logical positions. For example, the 8-digit LED
+     *    modules from diymore.cc have the left 4 and right 4 LED digits
+     *    swapped.
      */
     DualHc595Module(
         const T_SPII& spiInterface,
@@ -112,9 +135,14 @@ class DualHc595Module : public ScanningModule<
             segmentOnPattern /*elementOnPattern*/,
             digitOnPattern /*groupOnPattern*/,
             byteOrder,
-            remapArray
+            (remapArray ? mLogicalToPhysicalRemapArray : nullptr)
         )
-    {}
+    {
+      // Invert the mapping because LedMatrixDualHc595 requires the inverse
+      // mapping, in other words, logical postion to physical position.
+      internal::invertRemapArray(
+          mLogicalToPhysicalRemapArray, remapArray, T_DIGITS);
+    }
 
     void begin() {
       mLedMatrix.begin();
@@ -128,6 +156,9 @@ class DualHc595Module : public ScanningModule<
 
   private:
     LedMatrixDualHc595<T_SPII> mLedMatrix;
+
+    /** Mapping of logical digit positions to their physical positions. */
+    uint8_t mLogicalToPhysicalRemapArray[T_DIGITS];
 };
 
 } // ace_segment

src/ace_segment/max7219/Max7219Module.h
@@ -82,21 +82,35 @@ inline uint8_t convertPatternMax7219(uint8_t pattern) {
 } // internal
 
 /**
- * The 8-digit MAX7219 LED modules that I bought on eBay and Amazon are wired
- * such that digit 0 is the *right* most digit, and digit 7 is the *left* most
- * digit. This is the reverse of the convention used by the `LedDisplay` class.
- * Use this remap array to reverse the digit addresses by passing it into the
+ * A map of the physical digit position to its physical position. In other words
+ * `logicalPos = kDigitRemapArray[physicalPos]`. Pass this array into the
  * Max7219Module constructor.
+ *
+ * The 8-digit MAX7219 LED modules that I bought on eBay and Amazon are wired
+ * such that the digits appear as "7 6 5 4 3 2 1 0" intead of "0 1 2 3 4 5 6 7".
+ * This is the reverse of the convention used by the `LedDisplay` class.
  *
  * You can create your own remap array to handle other LED modules with
  * different physical ordering compared to the logical ordering.
  */
 extern const uint8_t kDigitRemapArray8Max7219[8];
 
+/**
+ * An implementation of LedModule using the MAX7219 chip. The chip uses SPI.
+ *
+ * @tparam T_SPII the class that implements the SPI interface, usually
+ *    either SoftSpiInterface or HardSpiInterface
+ * @tparam T_DIGITS number of digits in the module
+ */
 template <typename T_SPII, uint8_t T_DIGITS>
 class Max7219Module : public LedModule {
   public:
-    /** Constructor */
+    /**
+     * Constructor.
+     * @param spiInterface instance of T_SPII class
+     * @param remapArray (optional, nullable) a mapping of the physical digit
+     *    positions to their logical positions
+     */
     Max7219Module(
         const T_SPII& spiInterface,
         const uint8_t* remapArray = nullptr
@@ -155,19 +169,19 @@ class Max7219Module : public LedModule {
      *  * SW SPI Fast: 210 microseconds
      */
     void flush() {
-      for (uint8_t i = 0; i < T_DIGITS; ++i) {
-        uint8_t actualPos = remapDigit(i);
+      for (uint8_t physicalPos = 0; physicalPos < T_DIGITS; ++physicalPos) {
+        uint8_t logicalPos = remapPhysicalToLogical(physicalPos);
         uint8_t convertedPattern = internal::convertPatternMax7219(
-            mPatterns[i]);
-        mSpiInterface.send16(actualPos + 1, convertedPattern);
+            mPatterns[logicalPos]);
+        mSpiInterface.send16(physicalPos + 1, convertedPattern);
       }
 
       mSpiInterface.send16(kRegisterIntensity, mBrightness);
     }
 
   private:
-    /** Convert a logical position into the physical position. */
-    uint8_t remapDigit(uint8_t pos) const {
+    /** Convert a physical position into its logical position. */
+    uint8_t remapPhysicalToLogical(uint8_t pos) const {
       return mRemapArray ? mRemapArray[pos] : pos;
     }
 

commit b2e630e132476ef1f030595e5f17801ab7504d74

src/Tm1637Module.h: Fix flushIncremental() bug introduced in 0c37b29a which prevented mFlushStage from incrementing properly; add unit test for flush() and flushIncremental()

src/ace_segment/tm1637/Tm1637Module.h
@@ -29,6 +29,9 @@ SOFTWARE.
 #include <AceCommon.h> // incrementMod()
 #include "../LedModule.h"
 
+class Tm1637ModuleTest_flushIncremental;
+class Tm1637ModuleTest_flush;
+
 namespace ace_segment {
 
 /**
@@ -244,11 +247,12 @@ class Tm1637Module : public LedModule {
     void flushIncremental() {
       if (mFlushStage == T_DIGITS) {
         // Update brightness.
-        if (! isDirtyBit(T_DIGITS)) return;
-        mWireInterface.startCondition();
-        mWireInterface.sendByte(mBrightness);
-        mWireInterface.stopCondition();
-        clearDirtyBit(T_DIGITS);
+        if (isDirtyBit(T_DIGITS)) {
+          mWireInterface.startCondition();
+          mWireInterface.sendByte(mBrightness);
+          mWireInterface.stopCondition();
+          clearDirtyBit(T_DIGITS);
+        }
       } else {
         // Remap the logical position used by the controller to the actual
         // position. For example, if the controller digit 0 appears at physical
@@ -256,18 +260,18 @@ class Tm1637Module : public LedModule {
         // position 2 when sending the byte to controller digit 0.
         const uint8_t chipPos = mFlushStage;
         const uint8_t physicalPos = remapLogicalToPhysical(chipPos);
-        if (! isDirtyBit(physicalPos)) return;
+        if (isDirtyBit(physicalPos)) {
+          // Update changed digit.
+          mWireInterface.startCondition();
+          mWireInterface.sendByte(kDataCmdFixedAddress);
+          mWireInterface.stopCondition();
 
-        // Update changed digit.
-        mWireInterface.startCondition();
-        mWireInterface.sendByte(kDataCmdFixedAddress);
-        mWireInterface.stopCondition();
-
-        mWireInterface.startCondition();
-        mWireInterface.sendByte(kAddressCmd | chipPos);
-        mWireInterface.sendByte(mPatterns[physicalPos]);
-        mWireInterface.stopCondition();
-        clearDirtyBit(physicalPos);
+          mWireInterface.startCondition();
+          mWireInterface.sendByte(kAddressCmd | chipPos);
+          mWireInterface.sendByte(mPatterns[physicalPos]);
+          mWireInterface.stopCondition();
+          clearDirtyBit(physicalPos);
+        }
       }
 
       // An extra dirty bit is used for the brightness so use `T_DIGITS + 1`.
@@ -293,6 +297,10 @@ class Tm1637Module : public LedModule {
     }
 
   private:
+    // Give access to mIsDirty and mFlushStage
+    friend class ::Tm1637ModuleTest_flushIncremental;
+    friend class ::Tm1637ModuleTest_flush;
+
     static uint8_t const kDataCmdWriteDisplay = 0b01000000;
     static uint8_t const kDataCmdReadKeys =     0b01000010;
     static uint8_t const kDataCmdAutoAddress =  0b01000000;

src/ace_segment/testing/EventLog.h
@@ -38,6 +38,11 @@ enum class EventType : uint8_t {
   kSpiEnd,
   kSpiSend8,
   kSpiSend16,
+  kWireBegin,
+  kWireEnd,
+  kWireStartCondition,
+  kWireStopCondition,
+  kWireSendByte,
   kLedMatrixDraw,
   kLedMatrixEnableGroup,
   kLedMatrixDisableGroup,
@@ -77,6 +82,8 @@ class EventLog {
       mNumRecords++;
     }
 
+    //-------------------------------------------------------------------------
+
     void addSpiBegin() {
       if (mNumRecords >= kMaxRecords) return;
 
@@ -110,6 +117,51 @@ class EventLog {
       event.arg5 = value;
       mNumRecords++;
     }
+
+    //-------------------------------------------------------------------------
+
+    void addWireBegin() {
+      if (mNumRecords >= kMaxRecords) return;
+
+      Event& event = mEvents[mNumRecords];
+      event.type = EventType::kWireBegin;
+      mNumRecords++;
+    }
+
+    void addWireEnd() {
+      if (mNumRecords >= kMaxRecords) return;
+
+      Event& event = mEvents[mNumRecords];
+      event.type = EventType::kWireEnd;
+      mNumRecords++;
+    }
+
+    void addWireStartCondition() {
+      if (mNumRecords >= kMaxRecords) return;
+
+      Event& event = mEvents[mNumRecords];
+      event.type = EventType::kWireStartCondition;
+      mNumRecords++;
+    }
+
+    void addWireStopCondition() {
+      if (mNumRecords >= kMaxRecords) return;
+
+      Event& event = mEvents[mNumRecords];
+      event.type = EventType::kWireStopCondition;
+      mNumRecords++;
+    }
+
+    void addWireSendByte(uint8_t data) {
+      if (mNumRecords >= kMaxRecords) return;
+
+      Event& event = mEvents[mNumRecords];
+      event.type = EventType::kWireSendByte;
+      event.arg1 = data;
+      mNumRecords++;
+    }
+
+    //-------------------------------------------------------------------------
 
     void addLedMatrixDraw(uint8_t group, uint8_t elementPattern) {
       if (mNumRecords >= kMaxRecords) return;
@@ -146,6 +198,8 @@ class EventLog {
       event.type = EventType::kLedMatrixClear;
       mNumRecords++;
     }
+
+    //-------------------------------------------------------------------------
 
     uint8_t getNumRecords() { return mNumRecords; }
 
@@ -196,6 +250,24 @@ class EventLog {
           case EventType::kSpiSend16: {
               uint16_t value = va_arg(args, int);
               if (value != event.arg5) return false;
+            }
+            break;
+
+          case EventType::kWireBegin:
+            break;
+
+          case EventType::kWireEnd:
+            break;
+
+          case EventType::kWireStopCondition:
+            break;
+
+          case EventType::kWireStartCondition:
+            break;
+
+          case EventType::kWireSendByte: {
+              uint8_t value = va_arg(args, int);
+              if (value != event.arg1) return false;
             }
             break;
 

src/ace_segment/testing/TestableWireInterface.h
@@ -1,0 +1,38 @@
+#ifndef ACE_SEGMENT_TESTABLE_WIRE_INTERFACE_H
+#define ACE_SEGMENT_TESTABLE_WIRE_INTERFACE_H
+
+#include "EventLog.h" // EventLog
+
+namespace ace_segment {
+namespace testing {
+
+class TestableWireInterface {
+  public:
+    void begin() const {
+      mEventLog.addWireBegin();
+    }
+
+    void end() const {
+      mEventLog.addWireEnd();
+    }
+
+    void startCondition() const {
+      mEventLog.addWireStartCondition();
+    }
+
+    void stopCondition() const {
+      mEventLog.addWireStopCondition();
+    }
+
+    void sendByte(uint8_t data) const {
+      mEventLog.addWireSendByte(data);
+    }
+
+  public:
+    mutable EventLog mEventLog;
+};
+
+} // testing
+} // ace_segment
+
+#endif

tests/Tm1637ModuleTest/Tm1637ModuleTest.ino
@@ -9,10 +9,14 @@
 #include <Arduino.h>
 #include <AUnitVerbose.h>
 #include <AceSegment.h>
-#include <ace_segment/testing/TestableSpiInterface.h>
+#include <ace_segment/testing/TestableWireInterface.h>
+#include <ace_segment/testing/EventLog.h>
 
 using aunit::TestRunner;
+using ace_segment::testing::TestableWireInterface;
+using ace_segment::testing::EventType;
 using ace_segment::internal::initialDirtyBits;
+using ace_segment::Tm1637Module;
 
 //----------------------------------------------------------------------------
 
@@ -23,6 +27,150 @@ test(Tm1637ModuleTest, initialDirtyBits) {
   assertEqual(0b111, initialDirtyBits(3));
   assertEqual(0b1111111, initialDirtyBits(7));
   assertEqual(0b11111111, initialDirtyBits(8));
+}
+
+//----------------------------------------------------------------------------
+
+const uint8_t NUM_DIGITS = 4;
+TestableWireInterface wireInterface;
+Tm1637Module<TestableWireInterface, NUM_DIGITS> tm1637Module(wireInterface);
+
+test(Tm1637ModuleTest, flushIncremental) {
+  wireInterface.begin();
+  tm1637Module.begin();
+  wireInterface.mEventLog.clear();
+
+  // Verify dirty bits initial start dirty, 4 digit bits + 1 brightness bit
+  assertEqual(0x1F, tm1637Module.mIsDirty);
+  tm1637Module.mIsDirty = 0x0;
+
+  // Verify mFlushStage
+  assertEqual(0, tm1637Module.mFlushStage);
+
+  // Set digit 0.
+  tm1637Module.setPatternAt(1, 0x11);
+  assertEqual(0x1 << 1, tm1637Module.mIsDirty);
+
+  // Set brightness.
+  tm1637Module.setBrightness(2);
+  assertEqual((0x1 << NUM_DIGITS) | (0x1 << 1), tm1637Module.mIsDirty);
+
+  // Iteration 0 sends digit 0, which did not change, so send nothing.
+  assertEqual(0, tm1637Module.mFlushStage);
+  wireInterface.mEventLog.clear();
+  tm1637Module.flushIncremental();
+  assertEqual(0, wireInterface.mEventLog.getNumRecords());
+  assertFalse(tm1637Module.isDirtyBit(0));
+  assertEqual(1, tm1637Module.mFlushStage);
+
+  // Iteration 1 sends digit 1, which changed, so send digit 1;
+  assertEqual(1, tm1637Module.mFlushStage);
+  wireInterface.mEventLog.clear();
+  tm1637Module.flushIncremental();
+  assertEqual(7, wireInterface.mEventLog.getNumRecords());
+  assertTrue(wireInterface.mEventLog.assertEvents(
+    7,
+    (int) EventType::kWireStartCondition,
+    (int) EventType::kWireSendByte,
+        Tm1637Module<TestableWireInterface, NUM_DIGITS>::kDataCmdFixedAddress,
+    (int) EventType::kWireStopCondition,
+    (int) EventType::kWireStartCondition,
+    (int) EventType::kWireSendByte,
+        Tm1637Module<TestableWireInterface, NUM_DIGITS>::kAddressCmd | 0x1,
+    (int) EventType::kWireSendByte, 0x11,
+    (int) EventType::kWireStopCondition
+  ));
+  assertFalse(tm1637Module.isDirtyBit(1));
+  assertEqual(2, tm1637Module.mFlushStage);
+
+  // Iteration 2 sends digit 3, which sends nothing.
+  assertEqual(2, tm1637Module.mFlushStage);
+  wireInterface.mEventLog.clear();
+  tm1637Module.flushIncremental();
+  assertEqual(0, wireInterface.mEventLog.getNumRecords());
+  assertFalse(tm1637Module.isDirtyBit(2));
+  assertEqual(3, tm1637Module.mFlushStage);
+
+  // Iteration 3 sends digit 3, which sends nothing.
+  assertEqual(3, tm1637Module.mFlushStage);
+  wireInterface.mEventLog.clear();
+  tm1637Module.flushIncremental();
+  assertEqual(0, wireInterface.mEventLog.getNumRecords());
+  assertFalse(tm1637Module.isDirtyBit(3));
+  assertEqual(4, tm1637Module.mFlushStage);
+
+  // Iteration 4 sends brightness, which changed, so sends package.
+  assertEqual(4, tm1637Module.mFlushStage);
+  wireInterface.mEventLog.clear();
+  tm1637Module.flushIncremental();
+  assertEqual(3, wireInterface.mEventLog.getNumRecords());
+  assertTrue(wireInterface.mEventLog.assertEvents(
+    3,
+    (int) EventType::kWireStartCondition,
+    (int) EventType::kWireSendByte,
+        Tm1637Module<TestableWireInterface, NUM_DIGITS>::kBrightnessCmd
+          | Tm1637Module<TestableWireInterface, NUM_DIGITS>::kBrightnessLevelOn
+          | 2,
+    (int) EventType::kWireStopCondition
+  ));
+  assertFalse(tm1637Module.isDirtyBit(4));
+  assertEqual(0x0, tm1637Module.mIsDirty);
+  assertEqual(0, tm1637Module.mFlushStage);
+}
+
+test(Tm1637ModuleTest, flush) {
+  wireInterface.begin();
+  tm1637Module.begin();
+  wireInterface.mEventLog.clear();
+
+  // Verify dirty bits initial start dirty, 4 digit bits + 1 brightness bit
+  assertEqual(0x1F, tm1637Module.mIsDirty);
+  tm1637Module.mIsDirty = 0x0;
+
+  // Calling flush() when nothing is dirty returns immediately.
+  wireInterface.mEventLog.clear();
+  tm1637Module.flush();
+  assertEqual(0, wireInterface.mEventLog.getNumRecords());
+
+  // Set digit 0.
+  tm1637Module.setPatternAt(1, 0x11);
+  assertEqual(0x1 << 1, tm1637Module.mIsDirty);
+
+  // Set brightness.
+  tm1637Module.setBrightness(2);
+  assertEqual((0x1 << NUM_DIGITS) | (0x1 << 1), tm1637Module.mIsDirty);
+
+  // Calling flush() sends everything.
+  wireInterface.mEventLog.clear();
+  tm1637Module.flush();
+  assertEqual(13, wireInterface.mEventLog.getNumRecords());
+  assertTrue(wireInterface.mEventLog.assertEvents(
+    13,
+    // brightness
+    (int) EventType::kWireStartCondition,
+    (int) EventType::kWireSendByte,
+        Tm1637Module<TestableWireInterface, NUM_DIGITS>::kBrightnessCmd
+          | Tm1637Module<TestableWireInterface, NUM_DIGITS>::kBrightnessLevelOn
+          | 2,
+    (int) EventType::kWireStopCondition,
+
+    // auto increment mode
+    (int) EventType::kWireStartCondition,
+    (int) EventType::kWireSendByte,
+        Tm1637Module<TestableWireInterface, NUM_DIGITS>::kDataCmdAutoAddress,
+    (int) EventType::kWireStopCondition,
+
+    // send 4 digits
+    (int) EventType::kWireStartCondition,
+    (int) EventType::kWireSendByte,
+        Tm1637Module<TestableWireInterface, NUM_DIGITS>::kAddressCmd,
+    (int) EventType::kWireSendByte, 0x00,
+    (int) EventType::kWireSendByte, 0x11,
+    (int) EventType::kWireSendByte, 0x00,
+    (int) EventType::kWireSendByte, 0x00,
+    (int) EventType::kWireStopCondition
+  ));
+  assertEqual(0x0, tm1637Module.mIsDirty);
 }
 
 //----------------------------------------------------------------------------