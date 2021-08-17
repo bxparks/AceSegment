3e9845f266d8f68c2275cc174d478677177f8c7b
Brian Park <brian@xparks.net>
2021-08-17 14:39:13 -0700

    docs: Regenerate doxygen

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['hc595module_25',['Hc595Module',['../classace__segment_1_1Hc595Module.html',1,'ace_segment::Hc595Module&lt; T_SPII, T_DIGITS, T_SUBFIELDS, T_CI &gt;'],['../classace__segment_1_1Hc595Module.html#ae3ce4d5d32c92c01df7a424c6e5e56cc',1,'ace_segment::Hc595Module::Hc595Module()']]],
-  ['ht16k33module_26',['Ht16k33Module',['../classace__segment_1_1Ht16k33Module.html',1,'ace_segment::Ht16k33Module&lt; T_WIREI, T_DIGITS &gt;'],['../classace__segment_1_1Ht16k33Module.html#a0136851a409d3bba51c3cdf9aa4eb483',1,'ace_segment::Ht16k33Module::Ht16k33Module()']]],
+  ['ht16k33module_26',['Ht16k33Module',['../classace__segment_1_1Ht16k33Module.html',1,'ace_segment::Ht16k33Module&lt; T_WIREI, T_DIGITS &gt;'],['../classace__segment_1_1Ht16k33Module.html#af36ff9ba6dffb7c445cdf0b4c6a1761f',1,'ace_segment::Ht16k33Module::Ht16k33Module()']]],
   ['hybridmodule_27',['HybridModule',['../classace__segment_1_1HybridModule.html',1,'ace_segment']]]
 ];
