cc87cddd9be6068a68adf3d7f042579f0d34ba52
Brian Park <brian@xparks.net>
2021-08-24 15:13:04 -0700

    examples/Ht16k33Demo.ino: Add diagnostic warning message when https://github.com/stevemarple/SoftWire is selected, for consistency

--- examples/Ht16k33Demo/Ht16k33Demo.ino
+++ examples/Ht16k33Demo/Ht16k33Demo.ino
@@ -166,6 +166,7 @@ const uint8_t HT16K33_I2C_ADDRESS = 0x70;
   using WireInterface = TwoWireInterface<SoftwareWire>;
   WireInterface wireInterface(softwareWire);
 #elif WIRE_INTERFACE_TYPE == WIRE_INTERFACE_TYPE_SOFT_WIRE
+  #warning Using https://github.com/stevemarple/SoftWire
   #include <SoftWire.h>
   const uint8_t SOFT_WIRE_BUFFER_SIZE = 32;
   uint8_t rxBuffer[SOFT_WIRE_BUFFER_SIZE];
