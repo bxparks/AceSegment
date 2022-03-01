a4343909332edd4fb184e3d98886bb40db9686ea
Brian Park <brian@xparks.net>
2022-03-01 13:52:31 -0800

    docs: Regenerate doxygen

--- docs/html/search/all_c.js
+++ docs/html/search/all_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['renderfieldnow_43',['renderFieldNow',['../classace__segment_1_1ScanningModule.html#ac6a00935339bb99f7a13e514a7252c77',1,'ace_segment::ScanningModule']]],
-  ['renderfieldwhenready_44',['renderFieldWhenReady',['../classace__segment_1_1ScanningModule.html#a31c1fcc0f98a937137752914398de85b',1,'ace_segment::ScanningModule']]]
+  ['readbuttons_43',['readButtons',['../classace__segment_1_1Tm1637Module.html#a73fb704b87858583de50f61e52ff205a',1,'ace_segment::Tm1637Module::readButtons()'],['../classace__segment_1_1Tm1638AnodeModule.html#a7a7b28d3eb12629b522fd51a747a1ecf',1,'ace_segment::Tm1638AnodeModule::readButtons()'],['../classace__segment_1_1Tm1638Module.html#a8b8404c5c87a908aa2f81ef7af892d4b',1,'ace_segment::Tm1638Module::readButtons()']]],
+  ['renderfieldnow_44',['renderFieldNow',['../classace__segment_1_1ScanningModule.html#ac6a00935339bb99f7a13e514a7252c77',1,'ace_segment::ScanningModule']]],
+  ['renderfieldwhenready_45',['renderFieldWhenReady',['../classace__segment_1_1ScanningModule.html#a31c1fcc0f98a937137752914398de85b',1,'ace_segment::ScanningModule']]]
 ];
--- docs/html/search/classes_2.js
+++ docs/html/search/classes_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['gpiointerface_61',['GpioInterface',['../classace__segment_1_1GpioInterface.html',1,'ace_segment']]]
+  ['gpiointerface_63',['GpioInterface',['../classace__segment_1_1GpioInterface.html',1,'ace_segment']]]
 ];
